f960d673939f2f9bc8ed60f44d9d4c00c8991407
TheHadiAhmadi <thehadiahmadi@gmail.com>
2023-06-20 12:54:51 +0430

    add viewport header

--- views.js
+++ views.js
@@ -113,6 +113,18 @@ export function Layout(props, slots) {
   return View(
     {
       htmlHead: [
+        View({ tag: "meta", charset: "UTF-8" }),
+        View({
+          tag: "meta",
+          "http-equiv": "X-UA-Compatible",
+          content: "IE=edge",
+        }),
+        View({
+          tag: "meta",
+          name: "viewport",
+          content: "width=device-width, initial-scale=1.0",
+        }),
+
         View({
           component: "link",
           tag: "link",
